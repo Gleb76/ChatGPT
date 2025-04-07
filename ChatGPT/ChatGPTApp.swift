d22b095c4ddd29eb965fc4a024e2b2d0e7821380
Gleb Klyga <klyga90@bk.ru>
2025-04-07 15:01:44 +0300

    Initial commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Глеб Клыга on 7.04.25.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
